c75a626f6665f9d034f1f2d7a21942c44fc55e3f
palexisru <a_podorov@mail.ru>
2025-02-14 12:31:35 +0300

    AI examples added

---
+++ school/v01.00/allai/php/index.php
@@ -0,0 +1,20 @@
+<html lang="ru" class="is_adaptive" data-page-type="parking" data-panel-url="https://server187.hosting.reg.ru/manager">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="robots" content="index, follow">
+    <title>Туннельное моделирование</title>
+</head>
+
+<body lang="RU">
+<a name="top"/>
+<a href="http://chess100.ru/">Шахматы</a>
+
+<h2>Болталка</h2>
+
+<p>
+<a href="#top">в начало</a>
+
+<hr>
+<p align="center">&copy; Alexey Podorov, 2023</p>
+</body></html>
